72090a4d4c1cfdce4427d09a9e957e0b22001d41
Will Wayman <will.wayman@cazoo.co.uk>
2023-01-07 22:24:11 +0100

    parse input

--- day_16_proboscidea_volcanium.py
+++ day_16_proboscidea_volcanium.py
@@ -4,12 +4,28 @@ from example_inputs.day_16 import example_input
 from real_inputs.day_16 import real_input
 
 
-def parse_input(scan: str) -> list:
-    return scan.split("\n")
+class Valve:
+    def __init__(self, line: str) -> None:
+        all_valves = re.findall(r"[A-Z]{2}", line)
+        self.valve = all_valves.pop(0)
+        self.leads_to = all_valves
+        self.flow_rate = int(re.findall(r"\d+", line)[0])
+
+
+def parse_input(scan: str) -> list[Valve]:
+    valves = []
+    for line in scan.split("\n"):
+        valves.append(Valve(line=line))
+
+    return valves
 
 
 def main(scan: str):
-    print(parse_input(scan=scan))
+    valves = parse_input(scan=scan)
+    for valve in valves:
+        print(valve.valve)
+        print(valve.leads_to)
+        print(valve.flow_rate)
 
 
 print("example should return ")
